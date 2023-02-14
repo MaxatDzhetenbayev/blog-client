c73d9253ead9e3800ed7b3a8ea5429a8b3625e12
MaxatDzhetenbayev <yaboku18@mail.ru>
2023-02-14 18:04:25 +0600

    layouts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import Container from "@mui/material/Container";
 
-const App = () => {
-	return (
-		<div></div>
-	)
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Home />
+        {/*<FullPost />*/}
+        {/*<AddPost />*/}
+        {/*<Login />*/}
+        {/*<Registration />*/}
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
 
+import "./index.scss";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
-
-
